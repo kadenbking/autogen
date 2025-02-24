e1b09d7eaeb906a7136e8786228d981dc4f52bca
Kaden King <70766937+kadenbking@users.noreply.github.com>
2025-02-24 15:33:11 -0600

    fix: build script

--- python/packages/autogen-studio/build.sh
+++ python/packages/autogen-studio/build.sh
@@ -4,11 +4,8 @@
 pip install -r requirements.txt
 
 # Install frontend dependencies
-npm install -g gatsby-cli
 npm install --global yarn
 
 cd frontend
 yarn install
-yarn build
-
-gatsby clean && rmdir /s /q ..\\autogenstudio\\web\\ui 2>nul & (set \"PREFIX_PATH_VALUE=\" || ver>nul) && gatsby build --prefix-paths && xcopy /E /I /Y public ..\\autogenstudio\\web\\ui
+npx gatsby build --prefix-paths
